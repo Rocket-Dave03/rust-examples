f4b15db291ccc9687df4284ae33aed2a05ca41e9
Rocket_dave03 <rocketdave30@gmail.com>
2025-02-16 20:57:23 -0400

    Formating

--- src/bin/read-lines.rs
+++ src/bin/read-lines.rs
@@ -1,5 +1,5 @@
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("You wrote '{}'", line.unwrap());
-    }
+	for line in std::io::stdin().lines() {
+		println!("You wrote '{}'", line.unwrap());
+	}
 }
