a3bd09efa078c602c45a2c4767d938568324fef6
Rocket_dave03 <rocketdave30@gmail.com>
2025-02-18 19:13:44 -0400

    Proper error for read-line

--- src/bin/read-line.rs
+++ src/bin/read-line.rs
@@ -1,5 +1,8 @@
-fn main() {
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
 	let mut lines = std::io::stdin().lines();
-	let line = lines.next().unwrap();
+	let line = lines.next().ok_or("Unable to read stdin")?;
 	println!("You wrote '{}'", line.unwrap());
+	return Ok(());
 }
